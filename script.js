d69138c729ddad981b16c3fb5a6345ea6b9396c5
lateef sanusi <lateefsanusifgc@gmail.com>
2025-04-15 20:19:41 +0100

    dd

--- script.js
+++ script.js
@@ -57,7 +57,70 @@ window.addEventListener("scroll", function () {
       btn.innerHTML = "<i class='fa-solid fa-play'></i>"
     }
   });
-  
-  
 
+  let carousel = document.querySelector(".classic");
+  let next = document.querySelector(".fa-arrow-right");
+  let desc = document.querySelector(".description");
+
+  let number = 1; 
+
+ 
+
+  function changecarrosel() {
+    if (number > 3) {
+      number = 1
+    }
+    console.log(number);
+
+    if (number == 1) {
+      desc.innerHTML = `
+        <h3>Rashid <span class='hub'>Centre</span></h3>
+        <p>
+          Welcome to the ultimate Rashid hub! Whether you're a newcomer or a seasoned fighter, this site is designed to help you master Rashid in Street Fighter 6. Here, you'll find tips, strategies, combo guides, and character insights to elevate your gameplay and make the most of Rashid’s unique speed and mix-up potential. Get ready to take your Rashid skills to the next level!
+        </p>
+      `;
+      desc.classList.add("firstone")
+      desc.classList.remove("replay");
+      desc.classList.remove("achievement");
+    }
+    
+    if (number == 2) {
+      
+      desc.innerHTML= `
+      <a  href="https://youtube.com/playlist?list=PLYHo76jk0RQJ4H_YcMauE7v8j-kGVAVTR&si=K-P--B3SMYqLKMnX" target="_blank">
+        <h2 class="redbull">Big Bird Red Bull Kumite</h2>
+      </a>
+
+        
+      `;
+      desc.classList.add("achievement");
+      desc.classList.remove("replay");
+      desc.classList.remove("firstone")
+    }
+
+    if (number == 3) {
+      desc.innerHTML = `
+        <div class = 'leftthird'>
+        <h3>Watch High Level Rashid Replays</h3>
+        <a href= "https://youtube.com/playlist?list=PLvZ5t8JLwU9Jlu2BBhSkTAqj2SiIMGKdv&si=9JUnKpEmTaRTKw0P" target = "_blank">
+        <button>WATCH NOW <i class='fa-solid fa-play'></i></button>
+        </div>
+        </a>
+        <div class="rightthird"></div>
+        
+      `;
+
+      desc.style.padding = "none";
+
+      
+      desc.classList.add("replay")
+      desc.classList.remove("achievement");
+    }
+    number+=1
+  }
+
+next.addEventListener("click", changecarrosel) 
+
+  
+setInterval(changecarrosel, 8000 )
 
